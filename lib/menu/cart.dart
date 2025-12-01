161227d4a23a0b623f44205eb1a3c2897a947431
FiveHeisenberg <reynzimp@gmail.com>
2025-12-01 11:56:16 +0700

    Total Payment Cart - Done

--- lib/menu/cart.dart
+++ lib/menu/cart.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:projek_mobile/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -13,35 +13,95 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
-  late Future<List<dynamic>> _futureCart;
+  List<dynamic> cartItems = [];
+  bool isLoading = true;
+  String? userId;
+  int total = 0;
+
+  // FUNGSI MEMUAT ID USER
+  Future<void> _loadUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    int? saveId = prefs.getInt('id_user');
+
+    if (saveId != null) {
+      setState(() {
+        userId = saveId.toString();
+      });
+      fetchCart();
+    }
+  }
+
+  // FUNGSI API MENGAMBIL DATA KERANJANG
+  Future<void> fetchCart() async {
+    final url = Uri.parse("http://localhost/resto/get_cart.php");
+
+    try {
+      final response = await http.post(
+        url,
+        body: {'id_user': userId.toString()},
+      );
+
+      if (response.statusCode == 200) {
+        final jsonResponse = jsonDecode(response.body);
+        if (jsonResponse['success'] == true) {
+          setState(() {
+            total = 0;
+            cartItems = jsonResponse['cart'];
+            isLoading = false;
+          });
+
+          fetchTotal();
+        } else {
+          throw Exception("Gagal memuat keranjang");
+        }
+      } else {
+        throw Exception('Gagal menghubungi server');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error: $e"))
+      );
+    }
+  }
+
+  // FUNGSI API MENGHITUNG TOTAL PAYMENT
+  Future<void> fetchTotal() async {
+    if (userId == null) return;
+
+    final url = Uri.parse("http://localhost/resto/get_total.php");
+    try {
+      final response = await http.post(
+        url,
+        body: {'id_user': userId!},
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['success'] == true) {
+          int fetchedTotal = data['total'] is int
+            ? data['total']
+            : int.tryParse(data['total'].toString()) ?? 0;
+          
+          setState(() {
+            total = fetchedTotal;
+          });
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("GAGAL MEMUAT TOTAL PEMBAYARAN"))
+      );
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    _futureCart = fetchCart();
+    _loadUserId();
   }
-
-  Future<List<dynamic>> fetchCart() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int? idUser = prefs.getInt("id_user");
-
-    if (idUser == null) return [];
-
-    final url = Uri.parse("http://localhost/resto/get_cart.php");
-
-    final response = await http.post(url, body: {
-      "id_user": idUser.toString(),
-    });
-
-    var data = jsonDecode(response.body);
-
-    if (data['success'] == true) {
-      return data['cart']; // list item keranjang
-    }
-
-    return [];
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -51,56 +111,166 @@ class _CartState extends State<Cart> {
         backgroundColor: AppColors.secondWhite,
         title: Text("Keranjang"),
       ),
-      body: FutureBuilder(
-        future: _futureCart,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
 
-          final cartItems = snapshot.data!;
+      body: Column(
+        children: [
+          // LIST PRODUK KERANJANG
+          Expanded(
+            child: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+              padding: EdgeInsets.all(16),
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+                final item = cartItems[index];
+                String nama = item['nama_produk'] ?? 'Produk';
+                String harga = NumberFormat.currency(
+                  locale: 'id_ID',
+                  symbol: 'Rp ',
+                  decimalDigits: 0,
+                ).format(int.parse(item['harga']));
+                String jumlah = item['jumlah'] ?? '1';
 
-          if (cartItems.isEmpty) {
-            return Center(child: Text("Keranjang kosong"));
-          }
+                // TEMPLATE CARD
+                return Container(
+                  margin: EdgeInsets.only(bottom: 12),
+                  decoration: BoxDecoration(
+                    color: AppColors.secondWhite,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      )
+                    ]
+                  ),
 
-          return ListView.builder(
-            itemCount: cartItems.length,
-            itemBuilder: (context, index) {
-              final item = cartItems[index];
+                  child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Row(
+                      children: [
+                        
+                        // GAMBAR PRODUK
+                        Container(
+                          width: 60,
+                          height: 60,
+                          color: Colors.grey[300],
+                        ),
 
-              return ListTile(
-                leading: Image.network(
-                  "http://localhost${item['path_gambar']}",
-                  width: 60,
-                  height: 60,
-                  fit: BoxFit.cover,
-                ),
-                title: Text(item['nama_produk']),
-                subtitle: Text("Rp. ${item['harga']}"),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
+                        // INFORMASI PRODUK
+                        SizedBox(width: 12),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+
+                              // NAMA PRODUK
+                              Text(
+                                nama,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+
+                              // HARGA
+                              SizedBox(height: 5),
+                              Text(
+                                harga,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              )
+                            ],
+                          )
+                        ),
+
+                        // TOMBOL + DAN -
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+
+                              }, 
+                              icon: Icon(Icons.add),
+                            ),
+                            Text(jumlah),
+                            IconButton(
+                              onPressed: () {
+
+                              }, 
+                              icon: Icon(Icons.remove)
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }
+            )
+          ),
+
+          // PANEL PEMBAYARAN
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: AppColors.primaryGreen,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    // Tombol Tambah/Kurang
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {
-                        // kamu bisa lanjut implementasikan di sini
-                      },
+                    Text(
+                      "Total Payment",
+                      style: TextStyle(fontSize: 16),
                     ),
-                    Text(item['jumlah'].toString()),
-                    IconButton(
-                      icon: Icon(Icons.remove),
-                      onPressed: () {
-                        // kamu bisa lanjut implementasikan di sini
-                      },
-                    ),
+
+                    // TOTAL HARGA
+                    Text(
+                      NumberFormat.currency(
+                        locale: 'id_ID',
+                        symbol: 'Rp ',
+                        decimalDigits: 0,
+                      ).format(total),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
                   ],
                 ),
-              );
-            },
-          );
-        },
+
+                // TOMBOL CHECKOUT
+                SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+
+                    }, 
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.secondWhite,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      )
+                    ),
+                    child: Text(
+                      'Checkout',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: AppColors.secondBlack
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
